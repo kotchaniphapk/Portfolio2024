8819ebb3c9e5af6d2d791c43b4140c2292b320ed
kotchaniphapk <kotchanipha.pk@gmail.com>
2024-03-12 17:39:14 +0700

    :bug:Fix suppressHydrationWarning

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,7 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="light">
-      <body className={inter.className}>
+      <body className={inter.className} suppressHydrationWarning={true}>
         <Providers>
           {children}
         </Providers>
