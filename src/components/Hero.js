7b4226ee91506a167cd7651c1c1a6dff1a23d0e8
kotchaniphapk <kotchanipha.pk@gmail.com>
2024-03-12 16:59:08 +0700

    :bug:Fix scroll

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -96,9 +96,9 @@ function Hero() {
                     stroke="currentColor"
                   >
                     <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2.5"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2.5"
                       d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4"
                     ></path>
                   </svg>
